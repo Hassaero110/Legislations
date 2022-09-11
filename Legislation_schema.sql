a81cb8a63bc1692ad9d912e086aa5a4f4ff67733
Hassan Raza <hassan.raza3@nhs.net>
2022-09-11 22:41:00 +0100

    Adding test json files

--- Legislation_schema.sql
+++ Legislation_schema.sql
@@ -1,15 +1,3 @@
-CREATE TABLE dbo.legislations(
-	LegislationVersionId			int NOT NULL PRIMARY KEY,
-	LegislationSourceId				UNIQUEIDENTIFIER,
-	Title							nvarchar(max),
-	NativeTitle						nvarchar(max),
-	IssuingBodySourceId				UNIQUEIDENTIFIER,
-	JurisdictionSourceId			UNIQUEIDENTIFIER
-
-	FOREIGN KEY (IssuingBodySourceId) REFERENCES dbo.issuing_body (SourceId),
-	FOREIGN KEY (JurisdictionSourceId) REFERENCES dbo.jurisdiction (SourceId)
-);
-
 CREATE TABLE dbo.jurisdiction(
 	Id					int IDENTITY (1,1),
 	Name				nvarchar(255),
@@ -22,6 +10,18 @@ CREATE TABLE dbo.issuing_body(
 	Name				nvarchar(255),
 	SourceId			UNIQUEIDENTIFIER PRIMARY KEY
 
+);
+
+CREATE TABLE dbo.legislations(
+	LegislationVersionId			int NOT NULL PRIMARY KEY,
+	LegislationSourceId				UNIQUEIDENTIFIER,
+	Title							nvarchar(max),
+	NativeTitle						nvarchar(max),
+	IssuingBodySourceId				UNIQUEIDENTIFIER,
+	JurisdictionSourceId			UNIQUEIDENTIFIER
+
+	FOREIGN KEY (IssuingBodySourceId) REFERENCES dbo.issuing_body (SourceId),
+	FOREIGN KEY (JurisdictionSourceId) REFERENCES dbo.jurisdiction (SourceId)
 );
 
 CREATE TABLE dbo.part(
