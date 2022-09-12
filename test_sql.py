94091c9edf2e7f1bc93ffaf5771cff687b7a4088
Hassan Raza <hassan.raza3@nhs.net>
2022-09-12 16:59:19 +0100

    CODE RUB: Adding post_issuing_bodies_to_sql()

--- test_sql.py
+++ test_sql.py
@@ -1,10 +1,12 @@
+from genericpath import isfile
+from msilib.schema import Directory
 from typing import List
 import pyodbc
 from pyodbc import Connection
-
+import os
 from legislation import Legislation 
 
-
+directory = 'Legislation'
 
 def connect_to_database(
     server='localhost\SQLEXPRESS', 
@@ -26,28 +28,37 @@ def connect_to_database(
     return cnxn
 
 def run_sql_command(sql, database):
+    print("---------DEBUG-----------")
+    print(sql)
     cursor = database.cursor()
     return cursor.execute(sql)
 
-def post_data_to_sql(legislation_data: Legislation, database: Connection):
+def check_issuing_body_exists(*, sql_table='dbo.issuing_body', database):
+    result = get_issuing_body_from_sql(database=database)
+    SourceID_list =[]
 
-    print('Type =')
-    print(type(legislation_data))
+    row = result.fetchone() 
+    while row: 
+        SourceID_list.append(row[0])
+        row = result.fetchone()
 
-    #for _leg in legislation_data:
-    #
-    #    sql_command = (
-    #        f"INSERT INTO dbo.issuing_body (Name, SourceID)"
-    #        f"VALUES ({_leg.IssuingBodyName}, {_leg.IssuingBodySourceId});"
-    #    )
-#
-    #    run_sql_command(sql_command, database)
+    return SourceID_list
 
-def get_data_from_sql(*, sql_table='dbo.issuing_body', database: Connection):
+def post_issuing_bodies_to_sql(legislation_data: Legislation, database: Connection):
+    _leg = legislation_data
+
+    if _leg.IssuingBodySourceId.upper() not in check_issuing_body_exists(database=database):
+            sql_command = (
+                f"INSERT INTO dbo.issuing_body (Name, SourceID)"
+                f"VALUES ('{_leg.IssuingBodyName}', '{_leg.IssuingBodySourceId}');"
+            )
+
+            run_sql_command(sql_command, database)
+
+def get_issuing_body_from_sql(*, sql_table='dbo.issuing_body', database: Connection):
     #Sample select query
-    result = run_sql_command(f"SELECT * FROM {sql_table};", database)
+    result = run_sql_command(f"SELECT SourceID FROM {sql_table};", database)
     return result
-
 
 def json_to_sql():
     pass
@@ -63,28 +74,17 @@ def optional_no_sql_database_version():
 
 if __name__ == "__main__":
 
-    database = connect_to_database(database="legislation")
-    print(f"{type(database)}")
+    database = connect_to_database(database="master")
 
-    Leg = Legislation.fromJson('Legislation/Legislation_822.json')
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+    # checking if it is a file
+        if os.path.isfile(f) and f.endswith(".json"):
 
+            leg_list = Legislation.listFromJson(f)
+            x = leg_list[0]
+            post_issuing_bodies_to_sql(x, database=database)
+            
+            
 
-
-   # post_data_to_sql(3, database)
-   # result = get_data_from_sql(database=database)
-#
-   # row = result.fetchone() 
-   # while row: 
-   #     print(row[0], row[1], row[2])
-   #     row = result.fetchone()
-
-
-    #result = run_sql_command("SELECT * FROM dbo.issuing_body;", database)
-    #for i in [column[0] for column in result.description]:
-    #    print(i)
-
-
-
-
-
-
+    
